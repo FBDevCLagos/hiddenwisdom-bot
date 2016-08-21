86b6ab945c0cbbbbaa5c2b7e40f3bbf5bac9b6b2
Pyjac <oyebanji.jacob@andela.com>
2016-08-21 14:01:41 +0100

    (feat) Create Message Sending Service

---
+++ app/Services/FBMessageSender.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+use GuzzleHttp\Client;
+
+class FBMessageSender
+{
+    public function send($recipientId, $message)
+    {
+        $client = new Client(['base_uri' => 'https://graph.facebook.com/v2.6/']);
+        $client->request(
+            'POST',
+            'me/messages',
+            [
+                'query' => ['access_token' => getenv('PAGE_ACCESS_TOKEN')],
+                'json' => [
+                    'recipient' => [
+                        'id' => $recipientId
+                    ],
+                    'message' => $message
+                ]
+            ]
+        );
+    }
+}
